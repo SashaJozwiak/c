72571bd2fcd6f945349a2a4983ad34b5e24db647
SashaJozwaik <wmdz88@gmail.com>
2023-10-11 19:17:34 +0300

    feat: add one example in example.c file

--- example.c
+++ example.c
@@ -26,3 +26,20 @@ int	main(void)
   return 0;
 }
 
+//sum every digit
+
+#include <stdio.h>
+
+int	main() 
+{
+    int num;
+
+    scanf("%d", &num);
+
+    int sum = num/100 + num%100/10 + num%10;
+
+    printf("%d", sum);
+  
+  return 0;
+}
+
